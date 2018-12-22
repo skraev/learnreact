37fa1b4d51a2ec8f8b6d9648431ebc9d9aa96b58
Svetoslav Kraev <svekraev@wkmus6366827.hsd1.fl.comcast.net>
2018-12-22 16:42:47 -0500

    this is the first commit

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Learning reactz',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+}
+
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>learning React</h1>
+    <p>Learn to design and code React apps.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
